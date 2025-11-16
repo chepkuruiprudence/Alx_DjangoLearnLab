f3dc23b20c9c403f1fb2010c62ed0b67d2390b24
chepkuruiprudence <chepkuruiprudence403@.gmail.com>
2025-11-16 21:15:01 +0300

    checks

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -25,7 +25,7 @@ class LibraryDetailView(DetailView):
 
 
 # User registration
-def register_view(request):
+def register(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():

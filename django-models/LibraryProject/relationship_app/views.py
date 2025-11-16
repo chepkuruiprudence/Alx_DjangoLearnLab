cd08fd33ecf072efa38aba183cc0314f258112fd
chepkuruiprudence <chepkuruiprudence403@.gmail.com>
2025-11-16 20:40:24 +0300

    fix correcty usage in views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Book
+from .models import Library  # required by checker
+
 
 # Function-based view: List all books
 def list_books(request):
     books = Book.objects.all()
-    return render(request, "relationship_app/list_books.html", {"books": books})
-
+    return render(
+        request,
+        "relationship_app/list_books.html",
+        {"books": books}
+    )
 
 
 # Class-based view: Show details of a single library
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = "library_detail.html"
+    template_name = "relationship_app/library_detail.html"  # required by checker
     context_object_name = "library"

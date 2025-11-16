eb78c637b5bcc14cdd13b91f9287521d12fa5eeb
chepkuruiprudence <chepkuruiprudence403@.gmail.com>
2025-11-16 23:12:14 +0300

    csrf implem,entation

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,6 +2,13 @@ from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Book
 from .forms import BookForm
+from django.http import HttpResponse
+
+def my_view(request):
+    response = HttpResponse("Hello secure world")
+    response['Content-Security-Policy'] = "default-src 'self'"
+    return response
+
 
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):  # renamed from list_books
@@ -40,3 +47,21 @@ def delete_book(request, pk):
         book.delete()
         return redirect('book_list')  # update redirect
     return render(request, 'bookshelf/book_confirm_delete.html', {'book': book})
+
+def search_books(request):
+    query = request.GET.get('q', '')
+    # Use ORM filter with parameterized query, not string concatenation
+    books = Book.objects.filter(title__icontains=query)
+    return render(request, 'bookshelf/book_list.html', {'books': books, 'query': query})
+
+def add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():  # validates and sanitizes input
+            book = form.save(commit=False)
+            book.added_by = request.user
+            book.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'bookshelf/book_form.html', {'form': form})

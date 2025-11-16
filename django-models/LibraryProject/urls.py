768ddde88850e7a2db42b9016b4bb350c18cdfec
chepkuruiprudence <chepkuruiprudence403@.gmail.com>
2025-11-16 20:30:44 +0300

    create templat5es to organixze books

--- django-models/LibraryProject/urls.py
+++ django-models/LibraryProject/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from django.contrib import admin, list_books, LibraryDetailView
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]

0313bb7b2fae33f8dcab351c4ad008a64a2abce6
chepkuruiprudence <chepkuruiprudence403@gmail.com>
2025-11-23 11:32:24 +0300

    set u\p djasngo api project

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
+User = settings.AUTH_USER_MODEL
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 

90a271cf429a7a8a73e1b8fc15c197a6a5320f2f
chepkuruiprudence <chepkuruiprudence403@gmail.com>
2025-12-07 10:44:52 +0300

    Creating Blog Post Management Features

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
-from .forms import RegisterForm
+from .forms import RegisterForm, PostForm
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Post
 
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
@@ -52,4 +58,42 @@ def profile_view(request):
         request.user.save()
 
     return render(request, "blog/profile.html")
+
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/post_list.html"
+    context_object_name = "posts"
+    ordering = ['-published_date']
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/post_detail.html"
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = "blog/post_form.html"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = "blog/post_form.html"
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy("post-list")
+    template_name = "blog/post_confirm_delete.html"
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
 
